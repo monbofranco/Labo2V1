0839140fc74f6d247f17199aae5e82ac59a0fef0
monbofranco <monbofranco@hotmail.com>
2022-02-02 17:25:40 -0500

    w

--- sw.js
+++ sw.js
@@ -4,22 +4,22 @@ this.addEventListener('install', function(event) {
       caches.open('v1').then(function(cache) {
         console.log('[Service Worker] Mise en cache globale: app shell et contenu et tout et tout');
         return cache.addAll([
-            '/index.html',
-            '/style.css',
-            '/index.js',
-            '/banner1.jpg',
-            '/banneer2.webp',
-            '/beginnerguide.jpeg',
-            '/custom1.jpeg',
-            '/icones/icon-512x512.png',
-            '/bootstrap-5.1.3-dist/css/bootstrap.min.css',
-            '/icons-1.7.2/font/bootstrap-icons.css',
-            '/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
-            '/customv1.jpeg',
-            '/custom1.jpeg',
-            '/icone.jpg',
-            '/background.jpeg',
-            '/background.webp',
+            'index.html',
+            'style.css',
+            'index.js',
+            'banner1.jpg',
+            'banneer2.webp',
+            'beginnerguide.jpeg',
+            'custom1.jpeg',
+            'icones/icon-512x512.png',
+            'bootstrap-5.1.3-dist/css/bootstrap.min.css',
+            'icons-1.7.2/font/bootstrap-icons.css',
+            'bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
+            'customv1.jpeg',
+            'custom1.jpeg',
+            'icone.jpg',
+            'background.jpeg',
+            'background.webp',
             
         ]);
       })
@@ -48,3 +48,44 @@ this.addEventListener('install', function(event) {
   this.addEventListener('activate', (e) => {
   console.log('Service worker activated !');
 });
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function(reg) {
+      // registration worked
+      console.log('Enregsistrement réussi !. Scope is ' + reg.scope);
+  }).catch(function(error) {
+      // registration failed
+      console.log('Erreur ' + error);
+    });
+  };
+
+
+// Initialise la variable pour l'utiliser plus tard afin d'afficher une invitation a installer la PWA
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+// Empêcher la mini-barre d'information d'apparaître sur mobile si deja installée
+e.preventDefault();
+// Réserve l'événement pour qu'il puisse être déclenché plus tard
+deferredPrompt = e;
+// Informe l'utilisateur qu'il peut installer la PWA avec une fenêtre
+installApp();
+
+console.log(`'beforeinstallprompt' a été déclenché !`);
+
+// Fonction qui renvoie a l'installation de l'application quand le boutton "Installer" est cliqué !
+  appButton.addEventListener('click', function() {
+    deferredPrompt.prompt();
+  })
+
+});
+
+// Fonction qui affiche une fenêtre a l'ouverture de la page pour informer l'utilisateur de la possibilité d'installer cette PWA
+// Possible de faire autrement, mais le toast de bootstrap est assez simple a utiliser
+
+function installApp() {
+console.log('ça marche?');
+const fenetre = document.getElementById('message')
+const toast = new bootstrap.Toast(fenetre, {delay: 6000}) //reste affichée 6 secondes seulement
+toast.show();
+};
