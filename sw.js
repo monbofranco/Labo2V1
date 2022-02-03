4598c5a31da8543e321f3e8c00414cc1b5e14c34
monbofranco <monbofranco@hotmail.com>
2022-02-02 20:24:48 -0500

    f

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ this.addEventListener('install', function(event) {
           './customv1.jpeg',
           './custom1.jpeg',
           './background.jpeg',
-          './manifest.json',
+          'manifest.json',
           './icon-192x192.png',
           './icon-256x256.png',
           './icon-384x384.png',
